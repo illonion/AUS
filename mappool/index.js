5d35c8c0c0396dfc7c8b56a819753f90ef811c64
illonion <zhou.lawrence.yz@gmail.com>
2024-04-24 21:42:23 +1000

    Add logic to now playing section

--- mappool/index.js
+++ mappool/index.js
@@ -27,12 +27,25 @@ const redTeamStarsEl = document.getElementById("redTeamStars")
 const blueTeamStarsEl = document.getElementById("blueTeamStars")
 let currentBestOf, currentRedStars, currentBlueStars
 
+// Now Playing
+const nowPlayingBackgroundEl = document.getElementById("nowPlayingBackground")
+const nowPlayingTitleSongEl = document.getElementById("nowPlayingTitleSong")
+const nowPlayingDifficultyEl = document.getElementById("nowPlayingDifficulty")
+const nowPlayingStatsSRNumberEl = document.getElementById("nowPlayingStatsSRNumber")
+const nowPlayingStatsARNumberEl = document.getElementById("nowPlayingStatsARNumber")
+const nowPlayingStatsCSNumberEl = document.getElementById("nowPlayingStatsCSNumber")
+const nowPlayingStatsBPMNumberEl = document.getElementById("nowPlayingStatsBPMNumber")
+const nowPlayingStatsLENNumberEl = document.getElementById("nowPlayingStatsLENNumber")
+let currentId, currentMd5
+let mappoolMapFound = false
+
 // Chat
-const chatDisplay = document.getElementById("chatDisplay")
+const chatDisplayEl = document.getElementById("chatDisplay")
 let chatLength = 0
 
 socket.onmessage = async (event) => {
     const data = JSON.parse(event.data)
+    console.log(data)
 
     // Team Names
     if (currentRedTeamName !== data.tourney.manager.teamName.left) {
@@ -72,10 +85,42 @@ socket.onmessage = async (event) => {
         }
     }
 
+    if ((currentId !== data.menu.bm.id || currentMd5 !== data.menu.bm.md5) && data.menu.bm.id !== 0) {
+        mappoolMapFound = false
+        currentId = data.menu.bm.id 
+        currentMd5 = data.menu.bm.md5
+
+        nowPlayingBackgroundEl.style.backgroundImage = `url("https://assets.ppy.sh/beatmaps/${data.menu.bm.set}/covers/cover.jpg")`
+        nowPlayingTitleSongEl.innerText = `${data.menu.bm.metadata.artist} - ${data.menu.bm.metadata.title}`
+        nowPlayingDifficultyEl.innerText = `[${data.menu.bm.metadata.difficulty}]`
+
+        setWrapForTexts(nowPlayingTitleSongEl, 552.4)
+        setWrapForTexts(nowPlayingDifficultyEl, 548)
+
+        const currentMap = findMapInMappool(currentId)
+        if (currentMap) {
+            mappoolMapFound = true
+            nowPlayingStatsSRNumberEl.innerText = Math.round(parseFloat(currentMap.difficultyrating) * 100) / 100
+            nowPlayingStatsARNumberEl.innerText = Math.round(parseFloat(currentMap.ar) * 10) / 10
+            nowPlayingStatsCSNumberEl.innerText = Math.round(parseFloat(currentMap.cs) * 10) / 10
+            nowPlayingStatsBPMNumberEl.innerText = Math.round(parseFloat(currentMap.bpm))
+            displayLength(currentMap.songLength)
+        }
+    }
+
+    // Found map in mappool?
+    if (!mappoolMapFound) {
+        nowPlayingStatsSRNumberEl.innerText = data.menu.bm.stats.fullSR
+        nowPlayingStatsARNumberEl.innerText = data.menu.bm.stats.AR
+        nowPlayingStatsCSNumberEl.innerText = data.menu.bm.stats.CS
+        nowPlayingStatsBPMNumberEl.innerText = data.menu.bm.stats.BPM.common
+        displayLength(Math.round(data.menu.bm.time.full / 1000))
+    }
+
     if (chatLength !== data.tourney.manager.chat.length) {
         // Chat stuff
         // This is also mostly taken from Victim Crasher: https://github.com/VictimCrasher/static/tree/master/WaveTournament
-        (chatLength === 0 || chatLength > data.tourney.manager.chat.length) ? (chatDisplay.innerHTML = "", chatLength = 0) : null;
+        (chatLength === 0 || chatLength > data.tourney.manager.chat.length) ? (chatDisplayEl.innerHTML = "", chatLength = 0) : null;
         const fragment = document.createDocumentFragment()
 
         for (let i = chatLength; i < data.tourney.manager.chat.length; i++) {
@@ -109,11 +154,24 @@ socket.onmessage = async (event) => {
             fragment.append(chatMessageContainer)
         }
 
-        chatDisplay.append(fragment)
+        chatDisplayEl.append(fragment)
         chatLength = data.tourney.manager.chat.length
-        chatDisplay.scrollTo({
-            top: chatDisplay.scrollHeight,
+        chatDisplayEl.scrollTo({
+            top: chatDisplayEl.scrollHeight,
             behavior: 'smooth'
         })
     }
+}
+
+// Set text wrap class for title / song name and difficulty
+function setWrapForTexts(element, width) {
+    if (element.getBoundingClientRect().width > width) element.classList.add("nowPlayingTextWrap")
+    else element.classList.remove("nowPlayingTextWrap")
+}
+
+// Display length
+function displayLength(songLength) {
+    const minutes = Math.floor(songLength / 60)
+    const seconds = Math.floor(songLength % 60).toString().padStart(2, '0')
+    nowPlayingStatsLENNumberEl.innerText = `${minutes}:${seconds}`
 }
